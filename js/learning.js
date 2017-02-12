e28f55e5eab3cf1b739c24745d299c58df6811e0
KonstantinDogadaev <konstantindogadaev@gmail.com>
2017-02-12 21:53:19 +0500

    start learning

---
+++ js/learning.js
@@ -0,0 +1,50 @@
+/*
+  var name = "Konstantin";//имя пользователя
+  console.log("Hello, " + name);
+*/
+
+/*цикл*/
+/*
+var num = 2;
+var exp = 10;
+var result = 1;
+var count = 1;
+while( count <= exp ){
+  result *= num;
+  count++;
+}
+console.log ( result )
+*/
+
+
+/*
+//цикл вывода переменной
+var lines = 5;
+var line = "";
+var symbol = *;
+var count = 1;
+while( count <= lines ){
+  line += symbol;
+  console.log( line );
+  count++;
+}
+*/
+
+/*
+//цикл for
+for ( var i=0; i<10; i++ ){
+  console.log( i );
+}
+*/
+
+/*
+// ветвление (вывод четных чисел)
+for ( var i=0; i<10; i++ ){
+  if ( i == 0 )
+    console.log( i + " - zero" );
+  else if ( i % 2 == 0 )
+    console.log( i + " - even" );
+  else
+    console.log( i + " - odd" );
+}
+*/
